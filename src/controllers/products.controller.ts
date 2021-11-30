f3b7c37e07fe76c00cc70e4d8f09c20baada10a6
Yafté M <redyafte@gmail.com>
2021-11-30 15:22:16 -0600

    Start project :rocket:

---
+++ src/controllers/products.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  Post,
+  Body,
+  Put,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { ProductsService } from './../services/products.service';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private productsService: ProductsService) {}
+
+  @Post()
+  create(@Body() payload: any) {
+    return this.productsService.create(payload);
+  }
+
+  // @Get()
+  // getProducts(
+  //   @Query('limit') limit = 100,
+  //   @Query('offset') offset = 0,
+  //   @Query('brand') brand: string,
+  // ) {
+  //   return `Limit: ${limit}, Offset: ${offset}, Brand: ${brand}`;
+  // }
+
+  @Get()
+  getProducts() {
+    return this.productsService.findAll();
+  }
+
+  @Get('filter')
+  getProductFilter() {
+    return `Esto es un filter`;
+  }
+
+  @Get(':id')
+  getProduct(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() payload: any) {
+    return this.productsService.update(id, payload);
+  }
+
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.remove(id);
+  }
+}
